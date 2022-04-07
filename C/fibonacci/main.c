f691d62e4d4ed4e80133b391c7e5d85c1e17b460
Cenker Özkan <68559468+cenkerozkan@users.noreply.github.com>
2022-04-07 20:28:06 +0300

    Add files via upload

---
+++ C/fibonacci/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/* 
+Problem:
+
+    Input a number from user and then print fibonacci elements
+    as much as given number, or given amount. 
+
+    Example: 
+
+        Given number = 10
+
+        Output: 1 1 2 3 5 8 13 21 34 55
+*/
+
+int main()
+{
+    // Input 
+    int iUser;
+    // Fibonacci
+    int iSwap = 0;
+    int iSwap2 = 1;
+
+    printf("\nLength\n:   ");
+    scanf("%d", &iUser);
+
+    // To start.
+    printf("\n%d", iSwap);
+    printf("\n%d", iSwap2);
+
+    // Calculation
+    while (iUser != 2)
+    {
+        // According to fibonacci
+        printf("\n%d", (iSwap + iSwap2));
+        
+        iSwap2 = iSwap + iSwap2;
+        iSwap = iSwap2 - iSwap;
+
+        iUser--;
+    }
+    return 0;
+}
